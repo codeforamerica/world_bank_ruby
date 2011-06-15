4b8fa13dc7243cc4c0c95dd77814af06b5222540
Justin Stoller <justin.stoller@gmail.com>
2011-06-15 12:42:11 -0700

    pedantics

--- spec/world_bank_spec.rb
+++ spec/world_bank_spec.rb
@@ -2,7 +2,7 @@ require 'helper'
 
 describe WorldBank do
   context 'client' do
-    it "should be an instance of WorldBank::Client" do
+    it "is an instance of WorldBank::Client" do
       WorldBank.client.should be_a WorldBank::Client  
     end
   end
