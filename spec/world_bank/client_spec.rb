02e1be412e5152a71aba3107c6a62b3db232e22e
Justin Stoller <justin.stoller@gmail.com>
2011-06-20 15:09:06 -0700

    fixed format parameter

--- spec/world_bank/client_spec.rb
+++ spec/world_bank/client_spec.rb
@@ -8,50 +8,50 @@ describe WorldBank::Client do
   
   context 'topics' do
     it 'returns all of the topics for the world bank' do
-      stub_get('topics').
+      stub_get('topics?format=json').
         to_return(:status => 200, :body => fixture('topics.json'))
       test = @client.topics
-      a_get('topics').should have_been_made
+      a_get('topics?format=json').should have_been_made
       test[0][:total].should eql 18
     end    
   end
 
   context 'indicators' do
     it 'returns all of the indicators for the world bank' do
-      stub_get('indicators').
+      stub_get('indicators?format=json').
         to_return(:status => 200, :body => fixture('indicators.json'))
       test = @client.indicators
-      a_get('indicators').should have_been_made
+      a_get('indicators?format=json').should have_been_made
       test[0][:pages].should eql 82
     end
   end
 
   context 'countries' do
     it 'returns all of the countries for the world bank' do
-      stub_get('countries').
+      stub_get('countries?format=json').
         to_return(:status => 200, :body => fixture('countries.json'))
       test = @client.countries
-      a_get('countries').should have_been_made
+      a_get('countries?format=json').should have_been_made
       test[0][:pages].should eql 5
     end
   end
 
   context 'lending_types' do
     it 'returns all of the lending types for the world bank' do
-      stub_get('lendingTypes').
+      stub_get('lendingTypes?format=json').
         to_return(:status => 200, :body => fixture('lending_types.json'))
       test = @client.lending_types
-      a_get('lendingTypes').should have_been_made
+      a_get('lendingTypes?format=json').should have_been_made
       test[0][:total].should eql '4'
     end
   end
   
   context 'income_levels' do
     it 'returns all of the income levels for the world bank' do
-      stub_get('incomeLevels').
+      stub_get('incomeLevels?format=json').
         to_return(:status => 200, :body => fixture('income_levels.json'))
       test = @client.income_levels
-      a_get('incomeLevels').should have_been_made
+      a_get('incomeLevels?format=json').should have_been_made
       test[0][:total].should eql '9'
     end
   end
