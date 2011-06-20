47c27e5699e3214d6c7eaca40e0f76777fe0dfc1
Justin Stoller <justin.stoller@gmail.com>
2011-06-20 16:11:23 -0700

    added WorldBank::Source initialization

--- lib/world_bank/source.rb
+++ lib/world_bank/source.rb
@@ -2,11 +2,29 @@ module WorldBank
 
   class Source
   
+    attr_reader :raw, :id, :name, :description, :url
+
+    def self.client
+      @client ||= WorldBank::Client.new
+    end
     def self.all(client)
       client.query[:dirs] = ['sources']
       client.get_query
     end
   
+    def self.find(id)
+      client.query[:dirs] = ['sources', id.to_s]
+      result = client.get_query
+      new(result[1][0])
+    end
+
+    def initialize(values={})
+      @raw = values
+      @id = values['id']
+      @name = values['name']
+      @description = values['description']
+      @url = values['url']
+    end
   end
 
 end
---
+++ spec/world_bank/source_spec.rb
@@ -0,0 +1,38 @@
+require 'helper'
+
+describe WorldBank::Source do
+  context 'client' do
+    it 'return an new WorldBank::Client' do
+      client = WorldBank::Source.client
+      client.should be_a WorldBank::Client
+    end
+  end
+  context 'find' do
+    it 'returns a new Source object' do
+      stub_get('sources/21?format=json').
+        to_return(:status => 200, :body => fixture('source_21.json'))
+      @gem = WorldBank::Source.find 21
+      a_get('sources/21?format=json').should have_been_made
+      @gem.should be_a WorldBank::Source
+    end
+    context 'returned Source has' do
+      before do
+        stub_get('sources/21?format=json').
+          to_return(:status => 200, :body => fixture('source_21.json'))
+        @gem = WorldBank::Source.find 21
+      end
+      it 'an id' do
+        @gem.id.should eql '21'
+      end
+      it 'a name' do
+        @gem.name.should eql 'Global Economic Monitor (GEM) Commodities'
+      end
+      it 'a description' do
+        @gem.description.should eql ''
+      end
+      it 'a url' do
+        @gem.url.should eql ''
+      end
+    end
+  end
+end
