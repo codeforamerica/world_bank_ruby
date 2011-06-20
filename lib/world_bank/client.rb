e8d212a64a5eb03297c48a5a4f7039e5961eb308
Justin Stoller <justin.stoller@gmail.com>
2011-06-20 14:31:43 -0700

    changed require statements
    added get_query method to build params prior to getting

--- lib/world_bank/client.rb
+++ lib/world_bank/client.rb
@@ -1,39 +1,57 @@
 require 'faraday_middleware'
-require 'world_bank/source'
-require 'world_bank/income_level'
-require 'world_bank/lending_type'
-require 'world_bank/country'
-require 'world_bank/indicator'
-require 'world_bank/topic'
+require File.expand_path(File.join(File.dirname(__FILE__), '/source'))
+require File.expand_path(File.join(File.dirname(__FILE__), '/income_level'))
+require File.expand_path(File.join(File.dirname(__FILE__), '/lending_type'))
+require File.expand_path(File.join(File.dirname(__FILE__), '/country'))
+require File.expand_path(File.join(File.dirname(__FILE__), '/indicator'))
+require File.expand_path(File.join(File.dirname(__FILE__), '/topic'))
 
 module WorldBank
-
   class Client
 
-    def sources
-      WorldBank::Source.all
+    attr_accessor :query
+
+    def initialize(options={})
+      @format = options[:format] || 'json'
+      @query = {:params => {}, :dirs => []}
+      @query[:params][:format] = @format
     end
-    
+
+    def sources
+      WorldBank::Source.all(self)
+    end
+
     def income_levels
       WorldBank::IncomeLevel.all
     end
-    
+
     def lending_types
       WorldBank::LendingType.all
     end
-    
+
     def countries
       WorldBank::Country.all
     end
-    
+
     def indicators
       WorldBank::Indicator.all
     end  
-    
+
     def topics
       WorldBank::Topic.all
     end    
 
+    def get_query
+      @path = @query[:dirs].join('/')
+      @path += '?'
+      params = []
+      @query[:params].each do |key, value|
+        params << "#{key.to_s}=#{value.to_s}"
+      end
+      @path += params.join('&')
+      get(@path)
+    end
+    
     def get(path, headers={})
       response = connection.get do |request|
         request.url(path, headers)
@@ -48,7 +66,15 @@ module WorldBank
         connection.use Faraday::Request::UrlEncoded
         connection.use Faraday::Response::RaiseError
         connection.use Faraday::Response::Mashify
-        connection.use Faraday::Response::ParseJson
+        case @format.to_s.downcase
+          when 'json'
+            connection.use Faraday::Response::ParseJson
+          when 'jsonp'
+            connection.use Faraday::Response::ParseJson
+          when 'xml'
+            connection.use Faraday::Response::ParseXml
+          when 'raw'
+          end
         connection.adapter(Faraday.default_adapter)
       end
     end  
